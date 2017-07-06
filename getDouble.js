c2af1c04d081f1a1606c4c702dbf3432bb1e5750
Mark.Keller@Libertymutual.com <N0028930@lm.lmig.com>
2017-07-06 17:25:18 -0400

    07/06/2017 @ 05:25pm

---
+++ getDouble.js
@@ -0,0 +1,10 @@
+// Create a function that meets the following:
+// Function's name - getDouble
+// Function's purpose - return double of a given value
+// Input - given value
+// Output - the input x 2
+var numToDouble = prompt('Enter a number and I\'ll double it');
+function getDouble(numToDouble) {
+    return (numToDouble * 2);
+}
+console.log(getDouble(numToDouble));
